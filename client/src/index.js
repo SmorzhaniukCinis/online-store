d5cf5bf9cee2ce58f0bb883cf2465231a1ecab95
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-02-11 19:36:07 +0200

    start

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
-import React, {createContext} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import UserStore from "./store/UserStore";
-import DeviceStore from "./store/DeviceStore";
-
-export const Context = createContext(null)
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Context.Provider value={{
-        user: new UserStore(),
-        device: new DeviceStore(),
-    }}>
-        <App />
-    </Context.Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
