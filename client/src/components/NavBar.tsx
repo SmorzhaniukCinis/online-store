c609939168f71ef7016bd61c1a5d7a5ba89abdf0
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2022-02-15 17:54:39 +0200

    some update in navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,27 +1,40 @@
 import React, {useContext} from 'react';
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {Context} from "../index";
-import {SHOP_ROUTE} from "../utils/constants";
+import {ADMIN_ROUTE, SHOP_ROUTE} from "../utils/constants";
+import {observer} from "mobx-react-lite";
 
-export const NavBar = () => {
+export const NavBar = observer( () => {
 
-    const {users} = useContext(Context)
+    const {user} = useContext(Context)
+    console.log(user.isAuth)
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">Web-Store</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        
+                        <Nav.Link href={ADMIN_ROUTE}>Device</Nav.Link>
+                        <Nav.Link href={ADMIN_ROUTE}>Shop</Nav.Link>
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                    </Nav>
+                    {user?.isAuth
+                        ? <Nav>
+                            <Nav.Link href={ADMIN_ROUTE}>Device</Nav.Link>
+                            <Nav.Link href={ADMIN_ROUTE}>Shop</Nav.Link>
+                            <Nav.Link href={ADMIN_ROUTE}>More deets</Nav.Link>
+                            <Nav.Link href={ADMIN_ROUTE}>More deets</Nav.Link>
+                        </Nav>
+                        : <Nav>
+                            <Nav.Link href={ADMIN_ROUTE}>Device</Nav.Link>
+                            <Nav.Link href={ADMIN_ROUTE}>Shop</Nav.Link>
+                            <Nav.Link href={ADMIN_ROUTE}>More deets</Nav.Link>
+                            <Nav.Link  href={SHOP_ROUTE}>test</Nav.Link>
+                            <button onClick={()=>user.setIsAuth(true)}> test 2</button>
+                        </Nav>}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-    );
-};
+    )
+})
